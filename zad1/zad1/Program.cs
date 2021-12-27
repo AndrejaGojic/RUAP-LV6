4cf67e7db02cede9585498e0d58bf710ce07d1b9
AndrejaGojic <andreja.gojic@hotmail.com>
2021-12-27 12:46:30 +0100

    Koristenje vlastitih podataka

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -41,7 +41,7 @@ namespace CallRequestResponseService
                             new StringTable()
                             {
                                 ColumnNames = new string[] {"MPG", "Cyl", "Displacement", "Horsepower", "Weight", "Acceleration", "Year", "CountryCode", "Model"},
-                                Values = new string[,] {  { "0", "0", "0", "0", "0", "0", "0", "0", "value" },  { "0", "0", "0", "0", "0", "0", "0", "0", "value" },  }
+                                Values = new string[,] { { "22", "4", "140", "72", "2408", "19", "71", "1", "chevrolet vega (sw)" } }
                             }
                         },
                     },
